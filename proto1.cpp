d2e489e674749c209b4fedf4c06f7f26dd5fe7b2
Blake Ba <blake.elink@gmail.com>
2020-05-28 07:26:35 -0400

    build out proto1.cpp

--- proto1.cpp
+++ proto1.cpp
@@ -1,35 +1,63 @@
-
 #include "stdlibs.h"
+// link to raylib
+
 using namespace std;
+using Vec3 = Vector3<float>;
+using Color = int;
+
+struct Voxel { 
+    Vec3 pos;  //ex, y , theta
+    Color color;
+};
 
 struct Entity {
-  void Update() { }
+    virtual void Update(float fElapsed) { /* check for collision */ /* update pos */  }
+    virtual void Draw() { }
+    virtual ~Entity() { }  
+    private:
+    // should this be shared_ptr?
+    list<Voxel> vox;
+};
+
+
+struct Bullet : Entity {
+
+};
+
+struct Player : Entity {
+
+
+};
+
+struct Enemy : Entity {
+
 };
 
 struct Game {
-  vector<unique_ptr<Entity>> entities;
+    vector<unique_ptr<Entity>> entities;
+    float fElapsed;
 
-  void UpdateAll() {
-    for (auto & e : entities) { e.get()->Update(); } 
+    void UpdateAll() {
+        for (auto & e : entities) { e.get()->Update(fElapsed); } 
 
-  }
+    }
 
 };
 
 template<class T1, class T2> bool isIntersecting(T1& a, T2& b) {
-  return true;
+    return true;
 }
 
 void testCollision(Entity &e1, Entity &e2) {
-  if (!isIntersecting(e1, e2)) return;
-  // handle collision
+    if (!isIntersecting(e1, e2)) return;
+    // handle collision
 }
 
 //vector.erase(remove_if(begin(vec), end(vec)), [] (    ) { return o.destroyed; };, end(vec));
 
 int main() {
-  Game g;
-  g.UpdateAll();
-  return 0;
+    Game g;
+    g.UpdateAll();
+    return 0;
 }
 
