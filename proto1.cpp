37e98a85e158dc51059cce415d4b4c3a8f966d11
blakeb211 <blake.elink@gmail.com>
2020-05-28 14:31:27 -0400

    olcPixelEngine

--- proto1.cpp
+++ proto1.cpp
@@ -1,13 +1,19 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
 #include "stdlibs.h"
-// link to raylib
 
 using namespace std;
-using Vec3 = Vector3<float>;
-using Color = int;
+
+struct Vector3 {
+  float x, y, theta;
+};
+
+using Vec3 = Vector3; 
 
 struct Voxel { 
-    Vec3 pos;  //ex, y , theta
-    Color color;
+    Vec3 pos;  //x, y , theta
+    int color;
+    int health; // voxel health
 };
 
 struct Entity {
@@ -19,8 +25,14 @@ struct Entity {
     list<Voxel> vox;
 };
 
-
 struct Bullet : Entity {
+
+};
+struct B1 : Bullet {
+
+};
+
+struct B2 : Bullet {
 
 };
 
@@ -29,18 +41,19 @@ struct Player : Entity {
 
 };
 
-struct Enemy : Entity {
-
+struct Enemy: Entity {
 };
 
-struct Game {
-    vector<unique_ptr<Entity>> entities;
-    float fElapsed;
+struct E1: Enemy {
 
-    void UpdateAll() {
-        for (auto & e : entities) { e.get()->Update(fElapsed); } 
+};
+struct E2: Enemy {
 
-    }
+};
+struct E3: Enemy {
+
+};
+struct E4: Enemy {
 
 };
 
@@ -55,9 +68,49 @@ void testCollision(Entity &e1, Entity &e2) {
 
 //vector.erase(remove_if(begin(vec), end(vec)), [] (    ) { return o.destroyed; };, end(vec));
 
-int main() {
-    Game g;
-    g.UpdateAll();
+// GLOBALS
+static const int screenWidth{1024};
+static const int screenHeight = {768};
+static const int pixelSize = {3};
+static bool gameOver{false};
+static bool pause{false};
+
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		sAppName = "Prototype #2 - Blake's Space Invaders Clone";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		// Called once at the start, so create things here
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// called once per frame
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand()% 255));	
+		return true;
+	}
+};
+
+
+
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main()
+{
+  Example demo;
+  if (demo.Construct(screenWidth / pixelSize,screenHeight / pixelSize, pixelSize, pixelSize))
+    demo.Start();
+
     return 0;
 }
-
