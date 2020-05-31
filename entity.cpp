fdcc3c83c1e17dc553c24d02eee80a551f15606d
Blake Ba <blake.elink@gmail.com>
2020-05-31 19:32:54 -0400

    fixed linker error with extern

--- entity.cpp
+++ entity.cpp
@@ -1,4 +1,3 @@
-
 #include "globals.h"
 #include "builder.h"
 #include <SFML/Graphics.hpp>
--- globals.h
+++ globals.h
@@ -6,10 +6,12 @@ namespace G {
   //------------------------------------------------------------------------------------
   // Globals 
   //------------------------------------------------------------------------------------
+  // inline tells the compiler that only one instance of this variable / possibly a function should exist
+  // constexpr and const are inline by default.
   constexpr int screenWidth{1024}, screenHeight{768};
   const std::string screenTitle{"Shooter - Prototype #2"};
   constexpr float blockWidth{3.f}, bW{blockWidth};
   constexpr float countBlocksX{screenWidth / blockWidth},countBlocksY{screenHeight/blockWidth}; 
-  std::vector<std::shared_ptr<Entity>> entity;
+  extern std::vector<std::shared_ptr<Entity>> entity;
 
 } // globals namespace
