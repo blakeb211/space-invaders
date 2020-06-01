b293f00944757a4e6eddd988d84e1eba4d757cf7
blakeb211 <blake.elink@gmail.com>
2020-06-01 11:50:18 -0400

    work on level editor

--- level_editor/level_editor_main.cpp
+++ level_editor/level_editor_main.cpp
@@ -3,44 +3,67 @@
 #include "stdlibs.h"
 using namespace std;
 
+constexpr float kScreenWidth = 800.f;
+constexpr float kScreenHeight = 6.f * kScreenWidth / 9.f;
 
 class Example : public olc::PixelGameEngine
 {
   public:
-  Example()
-  {
-    out_file = fstream("level_data.txt", ios::app);
-    sAppName = "Level Editor";
-  }
-  // level editor vars
-  vector<pair<float,float>> path; 
-  fstream out_file;
+    Example()
+    {
+      out_file = fstream("level_data.txt", ios::app);
+      sAppName = "Level Editor";
+      input_timer = 0.f;
+    }
+    // level editor vars
+    vector<pair<float,float>> path; 
+    fstream out_file;
+    float input_timer;
+    const float timer_max{0.2f};
 
   public:
-  bool OnUserCreate() override
-  {
-    // Called once at the start, so create things here
-    return true;
-  }
+    bool OnUserCreate() override
+    {
+      // Called once at the start, so create things here
+      return true;
+    }
 
-  bool OnUserUpdate(float fElapsedTime) override
-  {
-    // called once per frame
-    // mouse down: start building up path 
-    // mouse up: clean path, save, reset
-    // draw current path
-    //Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand()% 255));	
-    return true;
-  }
+    // Called once per frame
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+      input_timer += fElapsedTime;
+      if (input_timer > timer_max) {
+        input_timer -= timer_max;
+        if (GetMouse(0).bPressed) { // get left button state
+        }
+        // mouse down: start building up path 
+        // mouse up: save, clear_path, reset
+      }
+
+      // draw current path
+      for(auto &i : path) {
+        FillCircle((int)round(i.first),(int)round(i.second), 3, olc::WHITE); 
+      }
+      return true;
+    }
+
+    float CalcDistance(pair<float, float> p1, pair<float, float> p2) {
+      return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+    }
+    
+    // clean the current path up from having too many points close together
+    void CleanPath() {
+
+    }
 };
 
 
 int main()
 {
   Example demo;
-  if (demo.Construct(256, 240, 4, 4))
+  if (demo.Construct(kScreenWidth / 3.f, kScreenHeight / 3.f, 3, 3))
     demo.Start();
-  
+
   demo.out_file.close();
   return 0;
 }
